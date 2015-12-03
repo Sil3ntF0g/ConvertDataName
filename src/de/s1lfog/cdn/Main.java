8a5eacbd053081e661b06ee6926aae3896baea73
Sebastian <sese.hartl@gmail.com>
2015-12-03 11:33:03 +0100

    Add Main (Test Git inclusion)

---
+++ src/de/s1lfog/cdn/Main.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package de.s1lfog.cdn;
+
+/**
+ * @author s1lfog
+ *
+ */
+public class Main {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.err.println("Err");
+	}
+}
